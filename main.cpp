3a837e480a94083f4ac6b4932e9d2408e42ccb42
Eddy Valverde <eddygarros@hotmail.com>
2021-08-17 00:02:27 -0600

    feature: add decimal2binary function

--- main.cpp
+++ main.cpp
@@ -2,16 +2,69 @@
 // Simple C++ program to display "Hello World"
  
 // Header file for input output functions
-#include<iostream>
+#include <iostream>
+#include <string>          
  
 using namespace std;
  
 // main function -
 // where the execution of program begins
+
+// Function to convert binary to decimal
+int binaryToDecimal(string n)
+{
+    string num = n;
+    int dec_value = 0;
+ 
+    // Initializing base value to 1, i.e 2^0
+    int base = 1;
+ 
+    int len = num.length();
+    for (int i = len - 1; i >= 0; i--) {
+        if (num[i] == '1')
+            dec_value += base;
+        base = base * 2;
+    }
+ 
+    return dec_value;
+}
+
+string decimalToBinary(int n) //long long
+{
+   int i = 0;
+   string result="";
+   while (n > 0) {
+   //   binaryNumber[i] = n % 2;
+      result = result + to_string(n % 2);
+      n = n / 2;
+   }
+   return result;
+}
+
+string DecimalToBinary(int n) {
+   int binaryNumber[100], num=n;
+   int i = 0;
+   string result="";
+   while (n > 0) {
+      binaryNumber[i] = n % 2;
+      n = n / 2;
+      i++;
+   }
+   for (int j = i - 1; j >= 0; j--)
+      result = result + to_string(binaryNumber[j]);
+   return result;
+}
+
 int main()
 {
     // prints hello world
     cout<<"Hello World";
+    string num = "10101001";
+    cout << binaryToDecimal(num) << endl;
+
+    cout << DecimalToBinary(169) << endl;
+
+    //DecimalToBinary(169);
      
     return 0;
 }
